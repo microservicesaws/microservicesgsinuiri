b8d0e404059dc0c276cd7f2c720266357e363358
gsinuiri-coder <gsinuiri@gmail.com>
2022-12-25 03:32:24 -0500

    refactor: add sysfrank

---
+++ sysfrank-service/src/main/java/com/gsm/charlie/inventory/resource/DirectResource.java
@@ -0,0 +1,21 @@
+package com.gsm.charlie.inventory.resource;
+
+import com.gsm.charlie.catalog.resource.ProductResource;
+import com.gsm.charlie.security.domain.model.User;
+import com.gsm.charlie.shared.domain.model.AuditModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+public class DirectResource extends AuditModel {
+    private Long id;
+    private Integer quantity;
+    private BigDecimal price;
+    private ProductResource product;
+    private User user;
+}
