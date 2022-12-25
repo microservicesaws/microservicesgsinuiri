440a572d47bfce4230caf2e78ca00fcf75861d5b
gsinuiri-coder <gsinuiri@gmail.com>
2022-12-24 21:03:35 -0500

    refactor: all

---
+++ admin-service/src/test/java/com/gsm/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
